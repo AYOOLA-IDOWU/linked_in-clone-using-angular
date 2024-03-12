f69351448f0bbc7b16656a9a4b7e1b290907ea5a
Ayoola Idowu Hannah <idowuayoola2021@gmail.com>
2024-03-12 16:14:31 +0100

    linked-in-clone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { SignuppageComponent } from './signuppage/signuppage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LandingpageComponent,SignuppageComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
